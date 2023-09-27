56f58ee177cb7c19192d5ddf5167dabbcc998401
madeleineberild <madeleine.berild@hotmail.com>
2023-09-27 11:28:06 +0200

    Print simple example file

--- matrix_to_rapid.py
+++ matrix_to_rapid.py
@@ -5,7 +5,8 @@ import basic_heart_matrix
 def main():
     print("Hello, Matrix to RAPID code!!")
     matrix = basic_heart_matrix.heart_matrix
-    print(matrix)
+    for row in matrix:
+        print(''.join(map(str, row)))
 
 if __name__ == "__main__":
     main()
