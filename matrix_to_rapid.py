af989b4064f47612d14dada2db518a0cdca0ae8c
madeleineberild <madeleine.berild@hotmail.com>
2023-09-27 10:54:16 +0200

    Setup basic files for matrix_to_RAPID part

---
+++ matrix_to_rapid.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+import basic_heart_matrix
+
+def main():
+    print("Hello, Matrix to RAPID code!!")
+    matrix = basic_heart_matrix.heart_matrix
+    print(matrix)
+
+if __name__ == "__main__":
+    main()
