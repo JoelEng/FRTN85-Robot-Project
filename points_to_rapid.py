f0997b1ceed07fe3d195b1849740430b53652359
madeleineberild <madeleine.berild@hotmail.com>
2023-10-18 14:42:45 +0200

    Update image of greta and update rapid code

--- points_to_rapid.py
+++ points_to_rapid.py
@@ -17,13 +17,13 @@ def points_to_rapid(path_list):
 
 def write_plane_calibration():
     OUTPUT.write("\tPROC plane()\n")
-    OUTPUT.write("\t\tMoveJ air_1,v10,z1,tool1\WObj:=Workobject_1;\n")
-    OUTPUT.write("\t\tSearchL \Stop, DI10_0, sp_1, paper_1, v10, tool1\WObj:=Workobject_1;\n")
-    OUTPUT.write("\t\tMoveJ air_2,v10,z1,tool1\WObj:=Workobject_1;\n")
-    OUTPUT.write("\t\tSearchL \Stop, DI10_0, sp_2, paper_2, v10, tool1\WObj:=Workobject_1;\n")
-    OUTPUT.write("\t\tMoveJ air_3,v10,z1,tool1\WObj:=Workobject_1;\n")
-    OUTPUT.write("\t\tSearchL \Stop, DI10_0, sp_3, paper_3, v10, tool1\WObj:=Workobject_1;\n")
-    OUTPUT.write("\t\tMoveJ air_3,v10,z1,tool1\WObj:=Workobject_1;\n")
+    OUTPUT.write("\t\tMoveJ air_1,v50,z1,tool1\WObj:=Workobject_1;\n")
+    OUTPUT.write("\t\tSearchL \Stop, DI10_0, sp_1, paper_1, v20, tool1\WObj:=Workobject_1;\n")
+    OUTPUT.write("\t\tMoveJ air_2,v50,z1,tool1\WObj:=Workobject_1;\n")
+    OUTPUT.write("\t\tSearchL \Stop, DI10_0, sp_2, paper_2, v20, tool1\WObj:=Workobject_1;\n")
+    OUTPUT.write("\t\tMoveJ air_3,v50,z1,tool1\WObj:=Workobject_1;\n")
+    OUTPUT.write("\t\tSearchL \Stop, DI10_0, sp_3, paper_3, v20, tool1\WObj:=Workobject_1;\n")
+    OUTPUT.write("\t\tMoveJ air_3,v50,z1,tool1\WObj:=Workobject_1;\n")
     OUTPUT.write("\t\tWorkobject_1.oframe := DefDframe (paper_1, paper_2, paper_3, sp_1, sp_2, sp_3);\n")
     OUTPUT.write("\tENDPROC\n")
 
@@ -39,9 +39,9 @@ def define_constants(path_list, OUTPUT):
         OUTPUT.write(target_string(name, p[0], p[1]))
 
     # Constants and vars for plane calibration
-    OUTPUT.write("\tCONST robtarget paper_1:=[[0,50,0],[1,0,0,0],[0,0,-2,0],[9E+09,9E+09,9E+09,9E+09,9E+09,9E+09]];\n")
-    OUTPUT.write("\tCONST robtarget paper_2:=[[100,100,0],[1,0,0,0],[0,0,0,0],[9E+09,9E+09,9E+09,9E+09,9E+09,9E+09]];\n")
-    OUTPUT.write("\tCONST robtarget paper_3:=[[0,150,0],[1,0,0,0],[0,0,0,0],[9E+09,9E+09,9E+09,9E+09,9E+09,9E+09]];\n")
+    OUTPUT.write("\tCONST robtarget paper_1:=[[0,50,15],[1,0,0,0],[0,0,-2,0],[9E+09,9E+09,9E+09,9E+09,9E+09,9E+09]];\n")
+    OUTPUT.write("\tCONST robtarget paper_2:=[[100,100,15],[1,0,0,0],[0,0,0,0],[9E+09,9E+09,9E+09,9E+09,9E+09,9E+09]];\n")
+    OUTPUT.write("\tCONST robtarget paper_3:=[[0,150,15],[1,0,0,0],[0,0,0,0],[9E+09,9E+09,9E+09,9E+09,9E+09,9E+09]];\n")
     OUTPUT.write("\tCONST robtarget air_1:=[[0,50,-100],[1,0,0,0],[0,0,-2,0],[9E+09,9E+09,9E+09,9E+09,9E+09,9E+09]];\n")
     OUTPUT.write("\tCONST robtarget air_2:=[[100,100,-100],[1,0,0,0],[0,0,0,0],[9E+09,9E+09,9E+09,9E+09,9E+09,9E+09]];\n")
     OUTPUT.write("\tCONST robtarget air_3:=[[0,150,-100],[1,0,0,0],[0,0,0,0],[9E+09,9E+09,9E+09,9E+09,9E+09,9E+09]];\n")
@@ -56,9 +56,10 @@ def define_paths(paths, OUTPUT, points):
         name = f"path{i}"
         path_names.append(name)
         OUTPUT.write(f"\tPROC {name}()\n")
-        OUTPUT.write(f"\t\tMoveJ {points[path[0]]},v100,z5,tool1\WObj:=Workobject_1;\n")
+        OUTPUT.write(f"\t\tMoveJ Offs({points[path[0]]},0,0,5),v100,z5,tool1\WObj:=Workobject_1;\n")
         for p in path:
-            OUTPUT.write(f"\t\tMoveL Offs({points[p]},0,0,15),v100,z1,tool1\WObj:=Workobject_1;\n")
+            if(p != 0):
+                OUTPUT.write(f"\t\tMoveL Offs({points[p]},0,0,5),v100,z1,tool1\WObj:=Workobject_1;\n")
         OUTPUT.write(f"\t\tMoveL {points[path[-1]]},v100,z1,tool1\WObj:=Workobject_1;\n")
         OUTPUT.write(f"\tENDPROC\n")
     return path_names
