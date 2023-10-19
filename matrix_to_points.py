2e01a898a686262715faa1ff5ab7e7dc024e9756
Daniel Ahlberg <daniel.ahlberg.4255@student.lu.se>
2023-10-19 10:10:48 +0200

    Fixed m to points

--- matrix_to_points.py
+++ matrix_to_points.py
@@ -1,7 +1,10 @@
+MIN_VALUE = 200
+MAX_VALUE = 30
+
 def only_zero_neighbours(matrix, x, y):
     neighbors = [(x+1, y), (x-1, y), (x, y+1), (x, y-1), (x+1, y+1), (x-1, y-1), (x-1, y+1), (x+1, y-1)]
 
-    return sum(matrix[z][w] for z, w in neighbors if 0 <= z < len(matrix) and 0 <= w < len(matrix[0])) == 0
+    return sum(matrix[z][w] for z, w in neighbors if 0 <= z < len(matrix) and 0 <= w < len(matrix[0])) <= MAX_VALUE
 
 def recursive_helper(matrix, row, col, path, drawing):
     if (only_zero_neighbours(matrix, row, col)):
@@ -13,72 +16,67 @@ def recursive_helper(matrix, row, col, path, drawing):
         path.append((row, col))
 
         try:
-            if matrix[row][col+1]:
+            if matrix[row][col+1] >= MIN_VALUE:
                 recursive_helper(matrix, row, col+1, path, drawing)
         except IndexError:
             pass
 
         try:
-            if matrix[row+1][col+1]:
+            if matrix[row+1][col+1] >= MIN_VALUE:
                 recursive_helper(matrix, row+1, col+1, path, drawing)
         except IndexError:
             pass
     
         try:
-            if matrix[row+1][col]:
+            if matrix[row+1][col] >= MIN_VALUE:
                 recursive_helper(matrix, row+1, col, path, drawing)
         except IndexError:
             pass
     
         try:
-            if matrix[row+1][col-1]:
+            if matrix[row+1][col-1] >= MIN_VALUE:
                 recursive_helper(matrix, row+1, col-1, path, drawing)
         except IndexError:
             pass
 
         try:
-            if matrix[row][col-1]:
+            if matrix[row][col-1] >= MIN_VALUE:
                 recursive_helper(matrix, row, col-1, path, drawing)
         except IndexError:
             pass
 
         # try:
-        #     if matrix[row-1][col-1]:
+        #     if matrix[row-1][col-1] >= MIN_VALUE:
         #         recursive_helper(matrix, row-1, col-1, path, drawing)
         # except IndexError:
         #     pass
     
         # try:
-        #     if matrix[row-1][col]:
+        #     if matrix[row-1][col] >= MIN_VALUE:
         #         recursive_helper(matrix, row-1, col, path, drawing)
         # except IndexError:
         #     pass
 
         # try:
-        #     if matrix[row-1][col+1]:
+        #     if matrix[row-1][col+1] >= MIN_VALUE:
         #         recursive_helper(matrix, row-1, col+1, path, drawing)
         # except IndexError:
         #     pass
 
 def matrix_to_points(matrix):
     SMALLEST_PATH = 2
-    PATH_CUTOFF = 40
     drawing = list()
 
     for row in range(len(matrix)):
         for column in range(len(matrix[0])):
-                if matrix[row][column]:
+                if matrix[row][column] >= MIN_VALUE:
                     path = list()
                     recursive_helper(matrix, row, column, path, drawing)
 
-    filtered_list = filter(lambda x: len(x) > SMALLEST_PATH, drawing)
+    sorted_list = sorted(drawing, key=len, reverse=True)
+    filtered_list = filter(lambda x: len(x) > SMALLEST_PATH, sorted_list)
 
     unique_sorted_list = []
-    [unique_sorted_list.append(x) for x in list(filtered_list) if x not in unique_sorted_list]
+    [unique_sorted_list.append(x) for x in filtered_list if x not in unique_sorted_list]
 
     return unique_sorted_list
-
-# def main():
-#     matrix_to_points(img.imread("greta_pro.jpg"))
-
-# main()
